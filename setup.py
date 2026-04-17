d6bbcd25ddbe0fd22933c916719bf24f88a567b7
Damjan Markovic <131394680+dzdjdyzan@users.noreply.github.com>
2026-04-17 23:20:08 +0200

    Update setup.py
    fixed windows setup

--- setup.py
+++ setup.py
@@ -3,9 +3,21 @@ import pybind11
 import numpy as np
 import sys
 
-extra_compile_args = ['-O3', '-std=c++17']
-if sys.platform == 'darwin':
-    extra_compile_args.append('-mmacosx-version-min=10.14')
+extra_compile_args = []
+extra_link_args = []
+
+if sys.platform == 'win32':
+    # MSVC: enable C++17, define M_PI, optimise
+    extra_compile_args.append('/std:c++17')
+    extra_compile_args.append('/O2')
+    extra_compile_args.append('/D_USE_MATH_DEFINES')   # defines M_PI
+    extra_compile_args.append('/EHsc')
+else:
+    # macOS / Linux
+    extra_compile_args.append('-O3')
+    extra_compile_args.append('-std=c++17')
+    if sys.platform == 'darwin':
+        extra_compile_args.append('-mmacosx-version-min=10.14')
 
 ext_module = Extension(
     'image_processor_cpp',
@@ -13,6 +25,7 @@ ext_module = Extension(
     include_dirs=[pybind11.get_include(), np.get_include()],
     language='c++',
     extra_compile_args=extra_compile_args,
+    extra_link_args=extra_link_args,
 )
 
 setup(
